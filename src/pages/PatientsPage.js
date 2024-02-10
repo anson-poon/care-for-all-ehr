b88e30e6662c998604a7e420409e35aa6ac238b4
Anson Poon <ansonpoon77@gmail.com>
2024-02-09 20:59:31 -0800

    Added navigation bar

---
+++ src/pages/PatientsPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function PatientsPage() {
+    // const navigate = useNavigate();
+    // const handleClick = () => {
+    //     navigate('/patients/new');
+    // };
+
+    return (
+        <div>
+            <h1>Patients</h1>
+            {/* <button onClick={handleClick}>Add Patient</button> */}
+        </div>
+    );
+}
+
+export default PatientsPage;
