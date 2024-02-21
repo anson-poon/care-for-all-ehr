6a539a25fe69c4e1da50ff656f028977f37bde39
Tony Nguyen <91217918+tonguyen9295@users.noreply.github.com>
2024-02-20 20:44:10 -0600

    updated typo data for provider data

---
+++ providerData.js
@@ -0,0 +1,94 @@
+const providerData = [
+  {
+    providerProfileID: 1,
+    title: 'MD',
+    specialty: 'Internal Medicine',
+    phoneNumber: '1112223333',
+    providerID: 1,
+    providerFirstName: 'Josh',
+    providerLastName: 'Adams'
+  },
+  {
+    providerProfileID: 2,
+    title: 'DO',
+    specialty: 'Hospice',
+    phoneNumber: '1113334444',
+    providerID: 2,
+    providerFirstName: 'Jacob',
+    providerLastName: 'Smith'
+  },
+  {
+    providerProfileID: 3,
+    title: 'PA',
+    specialty: 'Palliative Care',
+    phoneNumber: '1115556666',
+    providerID: 3,
+    providerFirstName: 'Lou',
+    providerLastName: 'Jacobs'
+  },
+  {
+    providerProfileID: 4,
+    title: 'NP',
+    specialty: 'Physical Medicine & Rehabilitation',
+    phoneNumber: '1116667777',
+    providerID: 4,
+    providerFirstName: 'Minh',
+    providerLastName: 'Le'
+  },
+  {
+    providerProfileID: 5,
+    title: 'MD',
+    specialty: 'Nephrology',
+    phoneNumber: '1117778888',
+    providerID: 5,
+    providerFirstName: 'Dixie',
+    providerLastName: 'Pixie'
+  },
+  {
+    providerProfileID: 6,
+    title: 'DO',
+    specialty: 'OB/GYN',
+    phoneNumber: '1118889999',
+    providerID: 6,
+    providerFirstName: 'Sushi',
+    providerLastName: 'Mendez'
+  },
+  {
+    providerProfileID: 7,
+    title: 'PA',
+    specialty: 'Psychiatry',
+    phoneNumber: '1119992222',
+    providerID: 7,
+    providerFirstName: 'Hickory',
+    providerLastName: 'Rick'
+  },
+  {
+    providerProfileID: 8,
+    title: 'NP',
+    specialty: 'Family Medicine',
+    phoneNumber: '2223334444',
+    providerID: 8,
+    providerFirstName: 'Maple',
+    providerLastName: 'Jane'
+  },
+  {
+    providerProfileID: 9,
+    title: 'MD',
+    specialty: 'Gastroenterology',
+    phoneNumber: '2224445555',
+    providerID: 9,
+    providerFirstName: 'Honey',
+    providerLastName: 'Bunches'
+  },
+  {
+    providerProfileID: 10,
+    title: 'DO',
+    specialty: 'Pulmonology',
+    phoneNumber: '2225556666',
+    providerID: 10,
+    providerFirstName: 'Greek',
+    providerLastName: 'Yogurt'
+  },
+];
+
+export default providerData;
