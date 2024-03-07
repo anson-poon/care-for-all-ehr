9f1f3908ac4815950762317cdbe41942f8702e31
Tony Nguyen <91217918+tonguyen9295@users.noreply.github.com>
2024-03-06 20:39:56 -0600

    patient index routes updated to have all routes working for patient index

--- server/routes/patientIndexRoutes.js
+++ server/routes/patientIndexRoutes.js
@@ -5,8 +5,8 @@ const db = require('../database/db-connector');
 
 router.get('/data', patientController.selectAll);
 router.get('/search', patientController.search);
-// router.get('/create' patientController.create);
-// router.get('/update' patientController.update);
-// router.get('/delete' patientController.delete);
+router.post('/create', patientController.create);
+router.put('/update/:patientID', patientController.update);
+router.delete('/delete/:patientID', patientController.delete);
 
 module.exports = router;
