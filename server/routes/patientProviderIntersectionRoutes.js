2853724f75d8b4224b620579b9431e7c8b172aad
Tony Nguyen <91217918+tonguyen9295@users.noreply.github.com>
2024-03-08 13:32:24 -0600

    patient provider intersection routes created

---
+++ server/routes/patientProviderIntersectionRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const patientProviderIntersectionController = require('../controllers/patientProviderIntersectionController');
+const db = require('../database/db-connector');
+
+router.get('/data', patientProviderIntersectionController.selectAll);
+router.get('/search', patientProviderIntersectionController.search);
+router.post('/create', patientProviderIntersectionController.create);
+router.put('/update/:patientID/:providerID', patientProviderIntersectionController.update);
+router.delete('/delete/:patientID/:providerID', patientProviderIntersectionController.delete);
+
+module.exports = router;
