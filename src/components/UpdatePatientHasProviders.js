a56c7fcb6d4e357af59c3fd275668ba55b669f49
Anson Poon <ansonpoon77@gmail.com>
2024-03-10 18:04:11 -0700

    Modified to use ID dropdown instead of input

--- src/components/UpdatePatientHasProviders.js
+++ src/components/UpdatePatientHasProviders.js
@@ -1,7 +1,7 @@
 // Component that allows for updating a record for Provider Index Page
 
 import React from 'react';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from "axios";
 import { useLocation, useNavigate } from 'react-router-dom';
 
@@ -10,6 +10,36 @@ function UpdatePatientHasProviders() {
   Code citation: Group 70 learned how to create an update page that passes input values to 
   Express app for delivering to MySql database from https://github.com/safak/youtube2022/tree/react-mysql
   */
+
+  // implement SELECT to obtain all records for Patient Provider Intersection
+  const [patientData, setPatientData] = useState([]);   // Initialize state to hold fetched data
+  const [providerData, setProviderData] = useState([]);   // Initialize state to hold fetched data
+
+  // Fetch data from the database
+  useEffect(() => {
+    fetchPatientData();
+    fetchProviderData();
+  }, []);
+
+  // function for fetching patient-index data
+  const fetchPatientData = async () => {
+    try {
+      const response = await axios.get('/patient-index/data');
+      setPatientData(response.data);
+    } catch (err) {
+      console.error('Error fetching data:', err);
+    }
+  };
+
+  // function for fetching provider-index data
+  const fetchProviderData = async () => {
+    try {
+      const response = await axios.get('/provider-index/data');
+      setProviderData(response.data);
+    } catch (err) {
+      console.error('Error fetching data:', err);
+    }
+  };
 
   // go back to patient index page after submission of updated data or cancelling update of data
   const goBackToPatientProviderIntersection = useNavigate();
@@ -47,11 +77,21 @@ function UpdatePatientHasProviders() {
         <h4>Update Provider Relationship</h4>
         <div className="form-row">
           <label for="newPatientID">Patient ID:</label>
-          <input type="text" name="newPatientID" id="newPatientID" onChange={setUpdateValues} />
+          <select name="newPatientID" id="newPatientID" onChange={setUpdateValues} required>
+            <option value="" selected disabled hidden>Choose Attribute</option>
+            {patientData.map((item, index) => (
+              <option value={item.patientID}>{item.patientID} ({item.patientFirstName} {item.patientLastName})</option>
+            ))}
+          </select>
         </div>
         <div className="form-row">
           <label for="newProviderID">Provider ID:</label>
-          <input type="text" name="newProviderID" id="newProviderID" onChange={setUpdateValues} />
+          <select name="newProviderID" id="newProviderID" onChange={setUpdateValues} required>
+            <option value="" selected disabled hidden>Choose Attribute</option>
+            {providerData.map((item, index) => (
+              <option value={item.providerID}>{item.providerID} ({item.providerFirstName} {item.providerLastName})</option>
+            ))}
+          </select>
         </div>
         <br />
         <button className="add-button" onClick={handleSubmissionOfUpdate}>Submit</button>                <button className="add-button" onClick={() => goBackToPatientProviderIntersection("/patient-provider-intersection")}>Cancel</button>
